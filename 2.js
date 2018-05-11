bf8bbca652ea1bba918690702692cc5f1b6f923b
Dzmitry Bashlak <dzmitry.bashlak@itechart-group.com>
2018-05-11 13:49:04 +0300

    Fix for a proper result

--- 2.js
+++ 2.js
@@ -1,14 +1,25 @@
-var objects = [
-	{A: 1, B: 2, C: 3},
-	{A:3, name: 2, C: 1},
-	{A: 0, B: 3, C: 2}
+var objects = [{
+        A: 1,
+        B: 2,
+        name: 'vasya'
+    },
+    {
+        A: 3,
+        name: 'Vasya',
+        C: 1
+    },
+    {
+        name: 'Dima',
+        B: 3,
+        C: 2
+    }
 ];
 
 console.log('Initial array:', objects);
 
 for (var i = 0; i < objects.length; i++) {
-        if (objects[i].name) {
-            console.log('Object with name key', objects[i]);
-        }
-        
+    if (objects[i].name == 'Vasya') {
+        console.log('Object with name Vasya:', objects[i]);
     }
+
+}
